af8b52eee6ef228b253985e14274ab379adf49c7
Giovanni Oséas <111138894+gitgiovanni@users.noreply.github.com>
2024-12-18 11:40:17 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-
-// Adiciona o evento de busca ao CPF
-document.getElementById('cpf').addEventListener('blur', buscarEmail);
-
-// Foca no campo CPF ao carregar a página
-window.onload = () => document.getElementById('cpf').focus();
